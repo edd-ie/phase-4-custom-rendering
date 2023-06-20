80312ca3343e4b55b6899ab3814ed5d5237eb52d
edd-ie <ogendoeddie@gmail.com>
2023-06-20 12:01:41 +0300

    completion of custom rendering

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,10 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: {Error: "Record not found"}, status: :unprocessable_entity
+    end
   end
-
 end
